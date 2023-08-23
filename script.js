d361b218d190fb8ec60c1a37909eadac3241fb59
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-23 15:22:24 +0100

    Create functions and start flowchart

--- script.js
+++ script.js
@@ -6,7 +6,37 @@ const add = (firstNumber,secondNumber) => firstNumber+secondNumber;
 const subtract = (firstNumber,secondNumber) => firstNumber-secondNumber;
 const multiply = (firstNumber,secondNumber) => firstNumber*secondNumber;
 const divide = (firstNumber,secondNumber) => firstNumber/secondNumber;
+const inputToDisplay = (input) =>
+input.addEventListener("click", function(e){
+    display.textContent += e.target.textContent;
+
+});
 
 const operate = function(firstNumber,operator,secondNumber){
-    
+
 }
+
+const numbersAndOperators = document.querySelectorAll(".numberOrOperator");
+const equals = document.querySelector("#equals");
+const reset = document.querySelector("#reset");
+let display = document.querySelector("#display");
+
+numberOrOperator.forEach(inputToDisplay);
+
+// hacer que display sea 0
+// si no hay nada en display (0) y el e.target.textContent 
+// es un número o "-", reemplazar 0 por el textContent
+
+// si hay solo números o un negativo al principio y el e.target.textContent es
+// un número, añadir a display
+
+// si el display es solo negativo y el e.target.textContent es un operador,
+// no hacer nada.
+
+//si el display es 0 y el textContent es un operador que no sea negativo,
+//usar 0 como firstNumber y el operador como operator
+
+//si el display es un número (puede tener negativo), y el textContent es
+//un operador, usar el display como firstNumber y el operador como operator
+
+
