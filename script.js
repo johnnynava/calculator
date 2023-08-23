be018c932a685e5e144ca0c4cbc9ce9b1b715e65
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-23 12:05:01 +0100

    Finish step 4

---
+++ script.js
@@ -0,0 +1,12 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+const add = (firstNumber,secondNumber) => firstNumber+secondNumber;
+const subtract = (firstNumber,secondNumber) => firstNumber-secondNumber;
+const multiply = (firstNumber,secondNumber) => firstNumber*secondNumber;
+const divide = (firstNumber,secondNumber) => firstNumber/secondNumber;
+
+const operate = function(firstNumber,operator,secondNumber){
+    
+}
