46f9dcb57a56d20b18c14bdfa218acaac076c3cc
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-23 16:41:21 +0100

    Finish flowchart and operate function
    Next is removing redundancy from the flowchart and then actually
    implementing it, after that create a solution for decimal numbers to
    work
    
    It might also be a good idea to update the firstNumber and
    secondNumber in real time? (need to think about this)

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 let firstNumber;
 let secondNumber;
 let operator;
+const button = document.querySelectorAll("button");
+const equals = document.querySelector("#equals");
+const reset = document.querySelector("#reset");
+let display = document.querySelector("#display");
 
 const add = (firstNumber,secondNumber) => firstNumber+secondNumber;
 const subtract = (firstNumber,secondNumber) => firstNumber-secondNumber;
 const multiply = (firstNumber,secondNumber) => firstNumber*secondNumber;
 const divide = (firstNumber,secondNumber) => firstNumber/secondNumber;
+
+const operate = function(firstNumber,operator,secondNumber){
+    if (operator===" + "){return add(firstNumber,secondNumber)}
+    else if (operator===" - "){return subtract(firstNumber,secondNumber)}
+    else if (operator===" x "){return multiply(firstNumber,secondNumber)}
+    else return divide(firstNumber, secondNumber);
+};
+
 const inputToDisplay = (input) =>
 input.addEventListener("click", function(e){
     display.textContent += e.target.textContent;
 
 });
+button.forEach(inputToDisplay);
 
-const operate = function(firstNumber,operator,secondNumber){
 
-}
 
-const numbersAndOperators = document.querySelectorAll(".numberOrOperator");
-const equals = document.querySelector("#equals");
-const reset = document.querySelector("#reset");
-let display = document.querySelector("#display");
-
-numberOrOperator.forEach(inputToDisplay);
 
 // hacer que display sea 0
+
+// si textContent es reset, resetear a 0
+
 // si no hay nada en display (0) y el e.target.textContent 
-// es un número o "-", reemplazar 0 por el textContent
+//es un número o "-", reemplazar 0 por el textContent
 
 // si hay solo números o un negativo al principio y el e.target.textContent es
 // un número, añadir a display
@@ -39,4 +47,20 @@ numberOrOperator.forEach(inputToDisplay);
 //si el display es un número (puede tener negativo), y el textContent es
 //un operador, usar el display como firstNumber y el operador como operator
 
+//si se presiona igual luego de solo un negativo al principio o luego 
+//del operator (sin secondNumber), regresar ERROR
+
+//si no hay nada luego del operator, y el textContent es - o un número, añadir a
+//display
+
+//si se presiona un operator luego del operator o un solo negativo, no hacer nada
+
+//si se presiona igual luego de solo firstNumber, regresar el mismo número al
+//display
+
+//si se presiona igual luego de un número que va luego del operator, regresar
+//resultado
+
+//si se presiona un operator luego de un número que va luego del operator,
+//regresar resultado + operator de textContent 
 
