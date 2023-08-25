b3f759f902d3c0abeef9e74564930cb9ca0b56fb
“johnnynava” <137064281+johnnynava@users.noreply.github.com>
2023-08-25 20:37:04 +0100

    Finish back-end code
    Just need to make it pretty with CSS now.

--- script.js
+++ script.js
@@ -2,20 +2,15 @@ let firstNumber;
 let secondNumber;
 let operator;
 let currentOperator;
+let display = document.querySelector("#display");
 
 const anyNumber = /[0-9]/; 
-const anyNumberButZero = /[1-9]/
-const anyOperator = /[-x÷\+]/;
-const anyOperatorButNegative = /[x÷\+]/;
-
 const numbers = document.querySelectorAll(".number");
 const equals = document.querySelector("#equals");
 const operators = document.querySelectorAll(".operator");
 const zero = document.querySelector("#zero");
 const decimal = document.querySelector("#decimal");
 const reset = document.querySelector("#reset");
-
-let display = document.querySelector("#display");
 
 const add = (firstNumber,secondNumber) => firstNumber+secondNumber;
 const subtract = (firstNumber,secondNumber) => firstNumber-secondNumber;
@@ -25,16 +20,29 @@ const divide = (firstNumber,secondNumber) => firstNumber/secondNumber;
 const operate = function(){
     firstNumber = +firstNumber;
     secondNumber = +secondNumber;
-    if (currentOperator ===" + "){return add(firstNumber,secondNumber)}
-    else if (currentOperator ===" - "){return subtract(firstNumber,secondNumber)}
-    else if (currentOperator ===" x "){return multiply(firstNumber,secondNumber)}
-    else return divide(firstNumber, secondNumber);
-};
+    switch (currentOperator){
+        case " + ":
+            return add(firstNumber,secondNumber);
+            break;
+        case " - ":
+            return subtract(firstNumber,secondNumber);
+            break;
+        case " x ":
+            return multiply(firstNumber,secondNumber);
+            break;
+        case " ÷ ":
+            return divide(firstNumber, secondNumber);
+            break;
+    }
+}
 
 numbers.forEach((button) => button.addEventListener("click", function(e){
     let buttonPressed = e.target.textContent
     if (display.textContent==="0"){
         display.textContent = buttonPressed;
+    }
+    else if (secondNumber === "0"){
+        return;
     }
     else {
         display.textContent += buttonPressed;
@@ -91,20 +99,11 @@ reset.addEventListener("click", (e) => {
     currentOperator = "";
 })
 
-
-// si textContent es igual (=):
-
-//
-
-//1. y se presiona luego de solo un negativo al principio o luego 
-//del operator  (sin secondNumber), regresar ERROR
-
-//2. y se presiona luego de solo firstNumber, regresar el mismo número al
-//display
-
-//3. y se presiona luego de un número que va luego del operator (secondNumber), 
-//regresar resultado
-
-// si textContent es cero (0):
-
-//1. si hay solo números, un negativo o un operator, añadir a display
+equals.addEventListener("click", (e) => {
+    if (secondNumber){
+        firstNumber = operate();
+        if (!Number.isInteger(firstNumber)){firstNumber = firstNumber.toFixed(2)};
+        secondNumber = 0;
+        display.textContent = firstNumber;
+    }
+})
